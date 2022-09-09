996500dbfbf0d92620989e00cb9202f51b39a246
poolodoo <49055484+poolodoo@users.noreply.github.com>
2022-09-09 16:34:36 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# react-ts-nbbfuq
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/react-ts-nbbfuq)
---
+++ index.tsx
@@ -0,0 +1,24 @@
+import * as React from 'react';
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+
+import App from './App';
+
+const rootElement = document.getElementById('root');
+const root = createRoot(rootElement);
+
+function tick() {
+  const element = (
+    <div>
+      <h1>Hello, World!</h1>
+      <h2>It is {new Date().toLocaleTimeString()}.</h2>
+    </div>
+  );
+  root.render(element);
+}
+
+setInterval(tick, 1000);
+
+root.render(
+    <App />
+);
